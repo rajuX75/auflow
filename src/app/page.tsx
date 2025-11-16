891284fe456a729767989f5496824d7e888be5c8
Raju <mdrajuhosen990@gmail.com>
2025-11-16 15:49:17 +0600

    feat: integrate TRPC with React Query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import prisma from '@/lib/db';
+import { caller } from '@/trpc/server';
 
 const Page = async () => {
-  const user = await prisma.user.findMany();
+  const user = await caller.getUser();
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
       {JSON.stringify(user)}
---
+++ src/trpc/routers/_app.ts
@@ -0,0 +1,9 @@
+import prisma from '@/lib/db';
+import { baseProcedure, createTRPCRouter } from '../init';
+export const appRouter = createTRPCRouter({
+  getUser: baseProcedure.query((opts) => {
+    return prisma.user.findMany();
+  }),
+});
+// export type definition of API
+export type AppRouter = typeof appRouter;
