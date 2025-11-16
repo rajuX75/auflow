1edda11425dc87e0be130dc1d6db1af8dca218b9
Raju <mdrajuhosen990@gmail.com>
2025-11-16 12:59:16 +0600

    feat: initialize database schema with User and Post models
    - Created migration file to set up User and Post tables with necessary fields and constraints.
    - Added unique index on User email.
    - Established foreign key relationship between Post and User.
    - Introduced Prisma schema file to define data models and database connection.
    - Updated main page to fetch and display users from the database.
    - Implemented database connection utility using Prisma Client.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
-export default function Home() {
-  return <div className="">Hello world!</div>;
-}
+import prisma from '@/lib/db';
+
+const Page = async () => {
+  const user = await prisma.user.findMany();
+  return (
+    <div className="min-h-screen min-w-screen flex items-center justify-center">
+      {JSON.stringify(user)}
+    </div>
+  );
+};
+
+export default Page;
