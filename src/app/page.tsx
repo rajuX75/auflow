fc22e5060ee7a946d031dac79736b287083921a6
Raju <mdrajuhosen990@gmail.com>
2025-11-17 15:23:21 +0600

    feat:  integrate Inngest functions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,39 @@
+'use client';
+
 import Logout from '@/components/logout';
-import { requireAuth } from '@/lib/auth-utils';
-import { caller } from '@/trpc/server';
+import { Button } from '@/components/ui/button';
+import { useTRPC } from '@/trpc/client';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
-const Page = async () => {
-  await requireAuth();
-
-  const data = await caller.getUser();
+const Page = () => {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const { data } = useQuery(trpc.getWorkflows.queryOptions());
+  const create = useMutation(
+    trpc.createWorkflow.mutationOptions({
+      onSuccess: () => {
+        queryClient.invalidateQueries(trpc.getWorkflows.queryOptions());
+      },
+    })
+  );
   return (
-    <div className="min-h-screen min-w-screen flex items-center justify-center">
-      Protected server components
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-      <Logout />
+    <div className="min-h-screen min-w-screen flex items-center justify-center bg-gray-50 p-8">
+      <div className="max-w-2xl w-full bg-white rounded-lg shadow-lg p-8 space-y-6">
+        <h1 className="text-3xl font-bold text-gray-900 text-center">
+          Protected Server Components
+        </h1>
+
+        <pre className="bg-gray-100 rounded-md p-4 overflow-auto text-sm border border-gray-200">
+          {JSON.stringify(data, null, 2)}
+        </pre>
+
+        <div className="flex gap-4 justify-center">
+          <Button disabled={create.isPending} onClick={() => create.mutate()}>
+            Create Workflow
+          </Button>
+          <Logout />
+        </div>
+      </div>
     </div>
   );
 };
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,10 +1,22 @@
+import { inngest } from '@/inngest/client';
 import prisma from '@/lib/db';
 import { createTRPCRouter, protectedProcedure } from '../init';
+
 export const appRouter = createTRPCRouter({
-  getUser: protectedProcedure.query(({ ctx }) => {
-    return prisma.user.findMany({
-      where: {
-        id: ctx.auth.user.id,
+  getWorkflows: protectedProcedure.query(({ ctx }) => {
+    return prisma.workflow.findMany();
+  }),
+  createWorkflow: protectedProcedure.mutation(async () => {
+    await inngest.send({
+      name: 'test/hello.world',
+      data: {
+        email: 'raju@mail.com',
+      },
+    });
+
+    return prisma.workflow.create({
+      data: {
+        name: 'test-workflow',
       },
     });
   }),
